82c14f56d23a95ac565134a765903d4c3d92b6e7
Anastasia <akroyl419@gmail.com>
2025-09-08 19:23:11 +0300

    start

---
+++ src/services/movieService.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+import type { Movie } from "../types/movie"
+
+interface MovieRes {
+    results: Movie[]
+}
+
+const API_TOKEN = import.meta.env.VITE_TMDB_TOKEN;
+ 
+export default async function fetchMovies(search: string): Promise<Movie[]> {
+    const getMovie = await axios.get<MovieRes>(`https://api.themoviedb.org/3/search/movie`, {
+        params: {
+            query: search,
+        },
+        headers: {
+            Authorization: `Bearer ${API_TOKEN}`,
+        }
+    });
+
+    return getMovie.data.results;
+}
